372e9e74dcaee9b6ac94ecc4c1221550d53d8ba8
ismael <ismontelongo@outlook.com>
2024-02-09 18:59:01 +0000

    1.4

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/api/whoami", function (req, res) {
   // console.log(JSON.stringify(req.headers));
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
-  res.send({ip: ip, language: language, software: software});
+  res.send({ipaddress: ip, language: language, software: software});
 })
 
 // listen for requests :)
